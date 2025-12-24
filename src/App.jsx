289fefc8c5b2b503b1df55b56622e3d9eec68bf2
Your Name <you@example.com>
2025-12-23 22:54:52 -0800

    test

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <main className="content">
+        <h1>Hello, React!</h1>
+        <p>Welcome to your new React application.</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
